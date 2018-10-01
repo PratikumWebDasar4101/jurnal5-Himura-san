c661d2f15ebed52fa766e636e66673a7d47cf725
Zahid Musthofainal Akhyar <32158770+Himura-san@users.noreply.github.com>
2018-10-01 10:07:52 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,33 @@
 <?php
+  if (isset($_POST['submit'])) {
+        $nim = $_POST['nim'];
+        $nama = $_POST['nama'];
+        $email = $_POST['email'];
 
+        $count_nim = strlen($nim);
+        $count_nama = strlen($nama);
 
-
-
-
+        if ($count_nim > 10) {
+            ?>
+            <script>
+                alert("NIM harus kurang dari 10..");
+                location = "registrasi.php";
+            </script>
+            <?php
+        } elseif ($count_nama > 25) {
+            ?>
+            <script>
+                alert("Nama harus kurang dari 25..");
+                location = "registrasi.php";
+            </script>
+            <?php
+        } else {
+            ?>
+            <script>
+                alert("NIM : <?php echo $nim;?> \nNama : <?php echo $nama;?> \nEmail : <?php echo $email;?>");
+                location = "registrasi.php";
+            </script>
+            <?php
+        }
+    }
 ?>
